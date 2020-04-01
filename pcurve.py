6a1b516a581a1ad9ad8f9cc9544c5bb677fd7da3
Stephen Zhang <syz@math.ubc.ca>
2020-04-01 16:43:10 -0700

    modified to return p_interp/s_interp

--- pcurve.py
+++ pcurve.py
@@ -8,6 +8,8 @@ class PrincipalCurve:
         self.k = k
         self.p = None
         self.s = None
+        self.p_interp = None
+        self.s_interp = None
         
     def project(self, X, p, s):
         '''
@@ -15,9 +17,10 @@ class PrincipalCurve:
         @param X: data
         @param p: curve points
         @param s: curve parameterisation
-        @returns: interpolating parameter values, sum of square distances
+        @returns: interpolating parameter values, projected points on curve, sum of square distances
         '''
         s_interp = np.zeros(X.shape[0])
+        p_interp = np.zeros(X.shape)
         d_sq = 0
         
         for i in range(0, X.shape[0]):
@@ -30,8 +33,10 @@ class PrincipalCurve:
             idx_min = np.argmin(dist_seg)
             q = seg_proj[idx_min] 
             s_interp[i] = (np.linalg.norm(q)/np.linalg.norm(p[idx_min + 1, :] - p[idx_min, :]))*(s[idx_min+1]-s[idx_min]) + s[idx_min]
+            p_interp[i] = (s_interp[i] - s[idx_min])*(p[idx_min+1, :] - p[idx_min, :]) + p[idx_min, :]
             d_sq = d_sq + np.linalg.norm(proj_dist[idx_min])**2
-        return s_interp, d_sq
+            
+        return s_interp, p_interp, d_sq
      
     def renorm_parameterisation(self, p):
         '''
@@ -44,7 +49,7 @@ class PrincipalCurve:
         s[1:] = np.cumsum(seg_lens)
         s = s/sum(seg_lens)
         return s
-        
+    
     def fit(self, X, p = None, w = None, max_iter = 10, tol = 1e-3):
         '''
         Fit principal curve to data
@@ -58,29 +63,32 @@ class PrincipalCurve:
         pca = sklearn.decomposition.PCA(n_components = X.shape[1])
         pca.fit(X)
         pc1 = pca.components_[:, 0]
-        if p == None:
+        if p is None:
             p = np.kron(np.dot(X, pc1)/np.dot(pc1, pc1), pc1).reshape(X.shape) # starting point for iteration
             order = np.argsort([np.linalg.norm(p[0, :] - p[i, :]) for i in range(0, p.shape[0])])
             p = p[order]
         s = self.renorm_parameterisation(p)
+        
+        p_interp = np.zeros(X.shape)
+        s_interp = np.zeros(X.shape[0])
         d_sq_old = np.Inf
         
         for i in range(0, max_iter):
-            s_interp, d_sq = self.project(X, p, s)
+            s_interp, p_interp, d_sq = self.project(X, p, s)
             
             if np.abs(d_sq - d_sq_old) < tol:
                 break
             d_sq_old = d_sq
             
             order = np.argsort(s_interp)
-            s_interp = s_interp[order]
-            X = X[order, :]
+            # s_interp = s_interp[order]
+            # X = X[order, :]
 
-            spline = [UnivariateSpline(s_interp, X[:, j], k = self.k, w = w) for j in range(0, X.shape[1])]
+            spline = [UnivariateSpline(s_interp[order], X[order, j], k = self.k, w = w) for j in range(0, X.shape[1])]
 
             p = np.zeros((len(s_interp), X.shape[1]))
             for j in range(0, X.shape[1]):
-                p[:, j] = spline[j](s_interp)
+                p[:, j] = spline[j](s_interp[order])
 
             idx = [i for i in range(0, p.shape[0]-1) if (p[i] != p[i+1]).any()]
             p = p[idx, :]
@@ -88,6 +96,6 @@ class PrincipalCurve:
             
         self.s = s
         self.p = p
+        self.p_interp = p_interp
+        self.s_interp = s_interp
         return
-    
-    
